f7b628e6d0246e8b9c10e3bdd31fada86ebb421a
Varun Ursekar <varun.ursekar@gmail.com>
2022-10-23 01:33:47 -0400

    added scipdf parsed jsons

---
+++ utils/aws_parser.py
@@ -0,0 +1,26 @@
+import os
+import scipdf
+import json
+from tqdm import tqdm
+
+path_to_pdfs = 'scraped_train_pdfs/'
+path_to_output = 'parsed_train_pdfs/'
+filenames = os.listdir(path_to_pdfs)
+
+for i in tqdm(range(len(filenames))):
+
+    filename = filenames[i]
+
+    if '.pdf' not in filename:
+        continue
+
+    path = os.path.join(path_to_pdfs, filename)
+
+    try:
+        article_dict = scipdf.parse_pdf_to_dict(path)
+
+        with open(os.path.join(path_to_output, filename[:-4] + '.json'), 'w') as f:
+            json.dump(article_dict, f)
+    except:
+        print("Failed to parse")
+        continue
