6e9b7a9a8d4113b07cf7ed3284a47ccea96c7f62
Varun Ursekar <varun.ursekar@gmail.com>
2022-10-25 21:51:45 -0400

    added notebooks for training

--- utils/lsconll_to_conll.py
+++ utils/lsconll_to_conll.py
@@ -2,8 +2,8 @@ import os
 import json
 import re
 
-path_to_tagged = 'data/scipdf_tagged/test/'
-path_to_output = 'data/final_data/auto/test/'
+path_to_tagged = 'data/final_dataset/manual/train/'
+path_to_output = 'data/final_dataset/manual_formatted/train/'
 
 filenames = os.listdir(path_to_tagged)
 
@@ -12,26 +12,21 @@ if not os.path.exists(path_to_output):
 
 for filename in filenames:
 
-    if '.json' not in filename:
+    if '.conll' not in filename:
         continue
 
-    outfilename = re.sub('.json', '.conll', filename)
+    outfilename = filename
 
     with open(os.path.join(path_to_tagged,filename)) as fin:
         with open(os.path.join(path_to_output, outfilename), 'w') as fout:
-            for sample in fin:
-                sample = json.loads(sample)
+            for line in fin:
 
-                text = sample['text'].split(' ')
-                entities = ['O' for _ in range(len(text))]
-
-                for start, end, et, word in sample['entities']:
-                    entities[start] = 'B-{}'.format(et)
-
-                    for idx in range(start+1, end):
-                        entities[idx] = 'I-{}'.format(et)
-
-                for idx in range(len(text)):
-                    fout.write(f'{text[idx]}\t{entities[idx]}\n')
-
-                fout.write('\n')
+                try:
+                    token, _, _, tag = line.strip().split(' ')
+                    fout.write(f'{token}\t{tag}\n')
+                except ValueError:
+                    try:
+                        token, tag = line.strip().split(' ')
+                        fout.write(f'{token}\t{tag}\n')
+                    except ValueError:
+                        fout.write('\n')
